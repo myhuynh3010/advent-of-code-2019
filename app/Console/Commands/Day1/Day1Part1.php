e03abf869033a85976bd26d2adcd1c51e48bfe90
My Huynh <thanhmy3010@gmail.com>
2021-12-17 11:50:09 +0700

    Initial

---
+++ app/Console/Commands/Day1/Day1Part1.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands\Day1;
+
+use Illuminate\Console\Command;
+
+class Day1Part1 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'day1:part1';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $path = storage_path('app/inputs/day1.txt');
+        $input = file($path);
+
+        dd($input);
+    }
+}
